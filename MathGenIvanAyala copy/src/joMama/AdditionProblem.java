b963b1098059d8ca6e11665fda1a9b67c5cbbac0
Ivanca19 <105860596+Ivanca19@users.noreply.github.com>
2022-06-01 15:31:53 -0400

    Update AdditionProblem.java

--- MathGenIvanAyala copy/src/joMama/AdditionProblem.java
+++ MathGenIvanAyala copy/src/joMama/AdditionProblem.java
@@ -1,16 +1,19 @@
 package joMama;
 
+// This is a child class of MathProblem that focuses on Addition Problems.
 public class AdditionProblem extends MathProblem{
     public AdditionProblem(int num) {
         setQuestNum(num);
         setAnswer(getFirstNum() + getSecondNum());
     }
 
+    // This method returns the question as a String.
     public String question(){
         return getQuestNum() + ". " + getFirstNum() + " + "
                 + getSecondNum() + " = _____";
     }
 
+    // This method returns the answered question, regardless if it is correct or incorrect as a String. Along with the correct answer.
     public String answered(){
         return "\n" + getQuestNum() + ". " + getFirstNum() + " + " + getSecondNum() + " = " + getImp()
                 + "\n Correct answer: "  + getAnswer();
